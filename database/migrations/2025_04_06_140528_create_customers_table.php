5eba73517d49d9478f5c6207176712a6b83acfd0
Weiam1 <145970015+Weiam1@users.noreply.github.com>
2025-04-06 19:40:17 +0200

    README

--- database/migrations/2025_04_06_140528_create_customers_table.php
+++ database/migrations/2025_04_06_140528_create_customers_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('costumer', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
+    $table->string('email')->unique();
+    $table->string('phone')->nullable();
+    $table->text('address')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('costumer');
+        Schema::dropIfExists('customers');
     }
 };
