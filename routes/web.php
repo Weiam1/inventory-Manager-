8669f741d654ccf7fc59e31a7313a641cedf70d3
Weiam1 <145970015+Weiam1@users.noreply.github.com>
2025-05-24 16:35:11 +0200

    Add Laravel Breeze auth scaffolding and route dashboard to products

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
+
+
+Route::get('/dashboard', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::resource('/products', ProductController::class);
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
